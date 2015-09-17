5cfafb3875dfd360ccf45bb46625459f8aac256b
Noah Heldman <nheldman@fairwaytech.com>
2015-09-17 15:31:03 -0700

    mt api

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 $(function() {
-	$('#CompanyUsers').html('this is good');
+	$.ajax({
+		url: '/@api/deki/users?dream.out.format=json',
+		data: {},
+		success: function(data, status, xhr) {
+			console.log(data);
+			$('#CompanyUsers').html(data);
+		},
+		error: function(xhr, status, message) {
+			alert(status + ': ' + message);
+		}
+	});
 });
