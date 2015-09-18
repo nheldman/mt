691a51925d1f0e0d45bb60111ee03da7a49b322a
Noah Heldman <nheldman@fairwaytech.com>
2015-09-17 20:57:10 -0700

    Add call to heroku

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 $(function() {
 	$.ajax({
 		url: '/@api/deki/users?dream.out.format=json',
-		data: {},
+		data: {
+			username: 'companyuser1',
+			email: 'noah.heldman+1@gmail.com',
+			fullname: 'Noah Heldman',
+			status: 'active'
+		},
+		method: 'POST'
 		success: function(data, status, xhr) {
 			console.log(data);
-			$('#CompanyUsers').html(data);
+		},
+		error: function(xhr, status, message) {
+			alert(status + ': ' + message);
+		}
+	});
+
+	$.ajax({
+		url: 'https://salty-dusk-6852.herokuapp.com/api/users',
+		success: function(data, status, xhr) {
+			console.log(data);
 		},
 		error: function(xhr, status, message) {
 			alert(status + ': ' + message);
